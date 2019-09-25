a299ccad718aac40b56101c61443359f441e51dd
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-09-25 20:43:26 -0300

    Devmind commit in 2019-09-25 20:43:25

--- mux4x1.vhd
+++ mux4x1.vhd
@@ -1,1 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity mux4x1 is
+	port(
+		
+		);
+end mux4x1;
+
+architecture proc_of_mux4x1 of mux4x1 is
+
+begin
+
+end proc_of_mux4x1;
