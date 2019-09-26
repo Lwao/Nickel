16466fa7df7c9b6a300bc2cfefc0d774dafa6536
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-09-25 21:32:07 -0300

    Devmind commit in 2019-09-25 21:32:06

--- or8.vhd
+++ or8.vhd
@@ -1,1 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity or8 is
+	port(
+	    in0, in1: in std_logic_vector (7 downto 0);
+	    output: out std_logic_vector (7 downto 0);
+	    Z, S, V: out std_logic
+		);
+end or8;
+
+architecture proc_of_or8 of or8 is
+    signal temp: std_logic_vector (7 downto 0);
+begin
+    temp <= in0 or in1;
+    Z <= '1' when temp="00000000" else '0';
+    S <= '1' when temp(7)='1' else '0';
+    V <= '1' when ((in0(7)='0' and in1(7)='0' and temp(7)='1') or (in0(7)='1' and in1(7)='1' and temp(7)='0')) else '0';
+    output <= temp;
+end proc_of_or8;
