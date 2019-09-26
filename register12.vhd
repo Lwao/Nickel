b869beeed58184709659229c8ff38402b4a87d58
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-09-25 21:08:37 -0300

    Devmind commit in 2019-09-25 21:08:37

--- register12.vhd
+++ register12.vhd
@@ -1,1 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity register11 is
+	port(
+	    input: in std_logic_vector (11 downto 0);
+	    clk, ld, clr: in std_logic;
+		output: out std_logic_vector (11 downto 0) 
+		);
+end mux4x1;
+
+architecture proc_of_register11 of register6 is
+
+begin
+    change: process(clk)
+        if (rising_edge(clk)) then
+            if (ld='1') then
+                output <= "000000000000";
+            elsif (ld='0' and clr='1') then
+                output <= input;
+            end if;
+        end if;
+    
+    end process change;
+end proc_of_register11;
