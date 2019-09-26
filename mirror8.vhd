9d582e5d5c3d702f6d67d85370b2e03027c9fff5
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-09-25 21:47:07 -0300

    Update mirror8.vhd

--- mirror8.vhd
+++ mirror8.vhd
@@ -1,1 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity and8 is
+	port(
+	    input: in std_logic_vector (7 downto 0);
+	    output: out std_logic_vector (7 downto 0);
+	    Z, S, V: out std_logic
+		);
+end and8;
+
+architecture proc_of_and8 of and8 is
+    signal temp: std_logic_vector (7 downto 0);
+begin
+    temp(7 downto 0) <= input(0 to 7);
+    Z <= '1' when temp="00000000" else '0';
+    S <= '1' when temp(7)='1' else '0';
+    V <= '1' when ((in0(7)='0' and in1(7)='0' and temp(7)='1') or (in0(7)='1' and in1(7)='1' and temp(7)='0')) else '0';
+    output <= temp;
+end proc_of_and8;
 
