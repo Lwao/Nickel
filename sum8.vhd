7fb9a1bd8b489c3dc56c7c5b428982acf6595a22
NicholasML97 <55857836+NicholasML97@users.noreply.github.com>
2019-09-27 14:51:02 -0300

    Update sum8.vhd

--- sum8.vhd
+++ sum8.vhd
@@ -8,7 +8,7 @@ entity sum8 is
 	    output: out std_logic_vector (7 downto 0);
 	    Z, S, V: out std_logic
 		);
-end and8;
+end sum8;
 
 architecture proc_of_sum8 of sum8 is
     signal temp: std_logic_vector (7 downto 0);
@@ -16,6 +16,6 @@ begin
     temp <= std_logic_vector(signed(in0) + signed(in1));
     Z <= '1' when temp="00000000" else '0';
     S <= '1' when temp(7)='1' else '0';
-    V <= '0';
+    V <= '1' when (temp(7)='1' and in0(7)='0' and in1(7)='0') or (temp(7)='0' and in0(7)='1' and in1(7)='1') else '0';
     output <= temp;
 end proc_of_sum8;
