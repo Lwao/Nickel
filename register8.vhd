a6711c6de4d6641bffb399d5b431c1779ced8279
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-09-27 13:09:24 -0300

    Devmind commit in 2019-09-27 13:09:24

--- register8.vhd
+++ register8.vhd
@@ -13,11 +13,13 @@ end register8;
 architecture proc_of_register8 of register8 is
 
 begin
-    change: process(clk)
+    change: 
+	 process(clk)
+    begin
         if (rising_edge(clk)) then
-            if (ld='1') then
+            if (clr='1') then
                 output <= "00000000";
-            elsif (ld='0' and clr='1') then
+            elsif (ld='1' and clr='0') then
                 output <= input;
             end if;
         end if;
