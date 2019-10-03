f893ff8cf19545de1f7d9420cea078c16a8d7347
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-10-02 21:21:36 -0300

    Add files via upload

--- ALU/ALU_components.vhd
+++ ALU/ALU_components.vhd
@@ -6,88 +6,77 @@ package ALU_components is
 	component sum8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component sum8;
 	
 	component sub8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component sub8;
 	
    component and8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component and8;
 	
 	component or8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component or8;
 	
 	component xor8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component xor8;
 	
 	component nand8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component nand8;
 	
 	component nor8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component nor8;
 	
 	component xnor8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component xnor8;
 	
 	component shiftl8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component shiftl8;
 	
 	component shiftr8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component shiftr8;
 	
 	component mirror8 is
 	port(
 	    input: in std_logic_vector (7 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component mirror8;
 
@@ -95,8 +84,7 @@ package ALU_components is
 	port(
 	    input: in std_logic_vector (7 downto 0);
        sel: in std_logic_vector (2 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component setbit8;
 	
@@ -104,8 +92,7 @@ package ALU_components is
 	port(
 	    input: in std_logic_vector (7 downto 0);
        sel: in std_logic_vector (2 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V: out std_logic
+	    output: out std_logic_vector (7 downto 0)
 		);
 	end component clrbit8;
 end package ALU_components;
--- ALU/alu.vhd
+++ ALU/alu.vhd
@@ -8,7 +8,7 @@ entity alu is
 	    rs1, rs2: in std_logic_vector (7 downto 0);
        mux_alu: in std_logic_vector (3 downto 0);
 	    output: out std_logic_vector (7 downto 0);
-	    L, G, E, Z, S, V: out std_logic
+	    L, G, E, Z, S, V, C: out std_logic
 		);
 end alu;
 
@@ -19,23 +19,25 @@ architecture proc_of_alu of alu is
     signal result_sla, result_sra, result_ror, result_rol: std_logic_vector (7 downto 0);
     signal result_mir, result_setb, result_clrb: std_logic_vector (7 downto 0);
 begin
-    SUM  : sum8    port map ();
-    SUB  : sub8    port map ();
-    AND_ : and8    port map ();
-    OR_  : or8     port map ();
-    XOR_ : xor8    port map ();
-    NAND_: nand8   port map ();
-    NOR_ : nor8    port map ();
-    XNOR_: xnor8   port map ();
-    SLA_ : shiftl8 port map ();
-    SRA_ : shiftr8 port map ();
-    ROL_ : xxx 	 port map ();
-    ROR_ : xxx 	 port map ();
-    MIR  : mirror8 port map ();
-    SETB : setbit8 port map ();
-    CLRB : clrbit8 port map ();
-    MUX  : mux16x8 port map (result_sum, result_sub, result_and, result_or,
+    SUM  : sum8     port map (rs1, rs2, result_sum);
+    SUB  : sub8     port map (rs1, rs2, result_sub);
+    AND_ : and8     port map (rs1, rs2, result_and);
+    OR_  : or8      port map (rs1, rs2, result_or);
+    XOR_ : xor8     port map (rs1, rs2, result_xor);
+    NAND_: nand8    port map (rs1, rs2, result_nand);
+    NOR_ : nor8     port map (rs1, rs2, result_nor);
+    XNOR_: xnor8    port map (rs1, rs2, result_xnor);
+    SLA_ : shiftl8  port map (rs1, rs2, result_sla);
+    SRA_ : shiftr8  port map (rs1, rs2, result_sra);
+    ROL_ : rotatel8 port map (rs1, rs2, result_rol);
+    ROR_ : rotater8 port map (rs1, rs2, result_ror);
+    MIR  : mirror8  port map (rs1, result_mir);
+    SETB : setbit8  port map (rs1, rs2(2 downto 0), result_setb);
+    CLRB : clrbit8  port map (rs1, rs2(2 downto 0), result_clrb);
+    MUX16: mux16x8  port map (result_sum, result_sub, result_and, result_or,
 			 result_xor, result_nand, result_nor, result_xnor,
 			 result_sla, result_sra, result_ror, result_rol,
 			 result_mir, result_setb, result_clrb, x"00", mux_alu, output);
+	 Z <= '1' when (output=x"00") else '0';
+	 S <= '1' when (output(7)='1') else '0';
 end proc_of_alu;
