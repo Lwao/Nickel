16a673e51b6231d58abb69d5eec8012d506a48a9
NicholasML97 <55857836+NicholasML97@users.noreply.github.com>
2019-10-07 11:04:10 -0300

    Update sum8.vhd

--- ALU/Components/sum8.vhd
+++ ALU/Components/sum8.vhd
@@ -6,24 +6,25 @@ entity sum8 is
 	port(
 	    in0, in1: in std_logic_vector (7 downto 0);
 	    output: out std_logic_vector (7 downto 0);
-		 C: out std_logic
+		 C, V: out std_logic
 		);
 end sum8;
 
 architecture proc_of_sum8 of sum8 is
-	 signal temp90, temp91, tempout: std_logic_vector (8 downto 0);
+	 signal temp0, temp1, tempout: std_logic_vector (8 downto 0);
 begin
 
 				  
-	 temp90(7 downto 0) <= in0;
-	 temp90(8) <= '0';
+	 temp0(7 downto 0) <= in0;
+	 temp0(8) <= '0';
 	 
-	 temp91(7 downto 0) <= in1;
-	 temp91(8) <= '0';
+	 temp1(7 downto 0) <= in1;
+	 temp1(8) <= '0';
 	 
-	 tempout <=std_logic_vector( unsigned(temp91)+unsigned(temp90));
+	 tempout <=std_logic_vector( unsigned(temp1)+unsigned(temp0));
 	 
 	 output <=tempout(7 downto 0);
 	 C <=tempout(8);
+ 	 V <= '1' when (tempout(7)='1' and in0(7)='0' and in1(7)='0') or (tempout(7)='0' and in0(7)='1' and in1(7)='1') else '0';
 				  
 end proc_of_sum8;
