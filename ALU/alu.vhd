80d131d747ba51be1cc31d03c2b6400789e4cabb
NicholasML97 <55857836+NicholasML97@users.noreply.github.com>
2019-10-07 11:14:14 -0300

    Update alu.vhd

--- ALU/alu.vhd
+++ ALU/alu.vhd
@@ -6,7 +6,7 @@ use work.ALU_components.all;
 entity alu is
 	port(
 	    rs1, rs2: in std_logic_vector (7 downto 0);
-       mux_alu: in std_logic_vector (3 downto 0);
+            mux_alu: in std_logic_vector (3 downto 0);
 	    output: out std_logic_vector (7 downto 0);
 	    L, G, E, Z, S, V, C: out std_logic
 		);
@@ -18,9 +18,10 @@ architecture proc_of_alu of alu is
     signal result_nand, result_nor, result_xnor: std_logic_vector (7 downto 0);
     signal result_sla, result_sra, result_ror, result_rol: std_logic_vector (7 downto 0);
     signal result_mir, result_setb, result_clrb: std_logic_vector (7 downto 0);
+    signal Csum, Vsum, Csub, Vsub: std_logic;
 begin
-    SUM  : sum8     port map (rs1, rs2, result_sum);
-    SUB  : sub8     port map (rs1, rs2, result_sub);
+    SUM  : sum8     port map (rs1, rs2, result_sum, Csum, Vsum);
+    SUB  : sub8     port map (rs1, rs2, result_sub, Csub, Vsub);
     AND_ : and8     port map (rs1, rs2, result_and);
     OR_  : or8      port map (rs1, rs2, result_or);
     XOR_ : xor8     port map (rs1, rs2, result_xor);
@@ -40,4 +41,11 @@ begin
 			 result_mir, result_setb, result_clrb, x"00", mux_alu, output);
 	 Z <= '1' when (output=x"00") else '0';
 	 S <= '1' when (output(7)='1') else '0';
+	 C <= Csum when mux_alu = "000" else
+	      Csub when mux_alu = "001" else
+	      '0';
+	 V <= Vsum when mux_alu = "000" else
+	      Vsub when mux_alu = "001" else
+	      '0';
+	    
 end proc_of_alu;
