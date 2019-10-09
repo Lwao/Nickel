57719269229bc0bd424457fcfae13999f0f87bed
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-10-09 11:37:34 -0300

    Update sum12.vhd

--- Lonely blocks/sum12.vhd
+++ Lonely blocks/sum12.vhd
@@ -6,16 +6,25 @@ entity sum12 is
 	port(
 	    in0, in1: in std_logic_vector (11 downto 0);
 	    output: out std_logic_vector (11 downto 0);
-	    Z, S, V: out std_logic
+		 C, V: out std_logic
 		);
-end sum8;
+end sum12;
 
 architecture proc_of_sum12 of sum12 is
-    signal temp: std_logic_vector (11 downto 0);
+	 signal temp0, temp1, tempout: std_logic_vector (12 downto 0);
 begin
-    temp <= std_logic_vector(signed(in0) + signed(in1));
-    Z <= '1' when temp="000000000000" else '0';
-    S <= '1' when temp(11)='1' else '0';
-    V <= '1' when (temp(11)='1' and in0(11)='0' and in1(11)='0') or (temp(11)='0' and in0(11)='1' and in1(11)='1') else '0';
-    output <= temp;
+
+				  
+	 temp0(11 downto 0) <= in0;
+	 temp0(12) <= '0';
+	 
+	 temp1(11 downto 0) <= in1;
+	 temp1(12) <= '0';
+	 
+	 tempout <=std_logic_vector( unsigned(temp1)+unsigned(temp0));
+	 
+	 output <=tempout(11 downto 0);
+	 C <=tempout(12);
+ 	 V <= '1' when (tempout(11)='1' and in0(11)='0' and in1(11)='0') or (tempout(11)='0' and in0(11)='1' and in1(11)='1') else '0';
+				  
 end proc_of_sum12;
