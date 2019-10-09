d6a33c1e00c47d40f216725f8257210a039f75b9
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2019-10-09 11:21:06 -0300

    Update Datapath.vhd

--- Datapath.vhd
+++ Datapath.vhd
@@ -5,47 +5,41 @@ use ieee.numeric_std.all;
 
 entity datapath is
 	port(
-	    rs1, rs2: in std_logic_vector (7 downto 0);
-            mux_alu: in std_logic_vector (3 downto 0);
-	    output: out std_logic_vector (7 downto 0);
-	    Z, S, V, C: out std_logic
+		clk: in	std_logic;
+		IR: in std_logic_vector(15 downto 0);
+		L, G, E, Z, S, V, C: out std_logic;
+		R1_ld, R2_ld, R3_ld, D_rd, D_wr: in std_logic;
+		PC_clr, PC_ld, I_ld, IR_ld, OUT_ld, IN_ld, SR_clr: in std_logic;
+		RS1_add, RS2_add, MUX_imm, MUX_sel, MUX_PC: in std_logic_vector (1 downto 0);
+		MUX_alu: out std_logic_vector(3 downto 0)
 		);
 end datapath;
 
-architecture proc_of_alu of datapath is
-    signal result_sum, result_sub: std_logic_vector (7 downto 0); 
-    signal result_and, result_or, result_xor: std_logic_vector (7 downto 0);
-    signal result_nand, result_nor, result_xnor: std_logic_vector (7 downto 0);
-    signal result_sla, result_sra, result_ror, result_rol: std_logic_vector (7 downto 0);
-    signal result_mir, result_setb, result_clrb: std_logic_vector (7 downto 0);
-    signal Csum, Vsum, Csub, Vsub: std_logic;
+architecture proc_of_datapath of datapath is
+	signal flags: std_logic_vector (7 downto 0);
 begin
-    SUM  : sum8     port map (rs1, rs2, result_sum, Csum, Vsum);
-    SUB  : sub8     port map (rs1, rs2, result_sub, Csub, Vsub);
-    AND_ : and8     port map (rs1, rs2, result_and);
-    OR_  : or8      port map (rs1, rs2, result_or);
-    XOR_ : xor8     port map (rs1, rs2, result_xor);
-    NAND_: nand8    port map (rs1, rs2, result_nand);
-    NOR_ : nor8     port map (rs1, rs2, result_nor);
-    XNOR_: xnor8    port map (rs1, rs2, result_xnor);
-    SLA_ : shiftl8  port map (rs1, rs2, result_sla);
-    SRA_ : shiftr8  port map (rs1, rs2, result_sra);
-    ROL_ : rotatel8 port map (rs1, rs2, result_rol);
-    ROR_ : rotater8 port map (rs1, rs2, result_ror);
-    MIR  : mirror8  port map (rs1, result_mir);
-    SETB : setbit8  port map (rs1, rs2(2 downto 0), result_setb);
-    CLRB : clrbit8  port map (rs1, rs2(2 downto 0), result_clrb);
-    MUX16: mux16x8  port map (result_sum, result_sub, result_and, result_or,
-			 result_xor, result_nand, result_nor, result_xnor,
-			 result_sla, result_sra, result_ror, result_rol,
-			 result_mir, result_setb, result_clrb, x"00", mux_alu, output);
-	 Z <= '1' when (output=x"00") else '0';
-	 S <= '1' when (output(7)='1') else '0';
-	 C <= Csum when mux_alu = "000" else
-	      Csub when mux_alu = "001" else
-	      '0';
-	 V <= Vsum when mux_alu = "000" else
-	      Vsub when mux_alu = "001" else
-	      '0';
-	    
+    flags(7) <= '0';
+    flags(6) <= L;
+    flags(5) <= G;
+    flags(4) <= E;
+    flags(3) <= Z;
+    flags(2) <= S;
+    flags(1) <= V;
+    flags(0) <= C;
+    COMP : comparator8 port map ();
+    ALU  : alu port map (rs1, rs2, mux_alu, alu_out);
+    R0   : register8 port map (x"00", clk, 1, 1, r0_out);
+    R1   : register8 port map (in, clk, ld, clr, r1_out);
+    R2   : register8 port map (in, clk, ld, clr, r2_out);
+    R3   : register8 port map (in, clk, ld, clr, r3_out);
+    IN   : register8 port map (in, clk, ld, clr, rin_out);
+    OUT  : register8 port map (in, clk, ld, clr, rout_out);
+    SR   : register8 port map (flags, clk, 1, SR_clr, out);
+    PC   : register12 port map (in, clk, ld, clr, out);
+    IR   : register12 port map (in, clk, ld, clr, out);
+    MUX_rs1 : mux4x8 port map (1, 2, 3, 4, sel, o);
+    MUX_rs2 : mux4x8 port map (1, 2, 3, 4, sel, o);
+    MUX_imm : mux4x8 port map (1, 2, 3, 4, sel, o)
+    MUX_sel : mux4x8 port map (rout_out, rin_out, rd_dt, alu_out, sel, o);
+    
 end proc_of_datapath;
